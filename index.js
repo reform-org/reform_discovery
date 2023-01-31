3bd811d012e09983096637e37a24312bbc39d514
lukasschreiber <info@lukasschreiber.com>
2023-01-31 23:42:47 +0100

    notify client about closed connections

--- index.js
+++ index.js
@@ -302,6 +302,7 @@ wss.on('connection', function connection(ws) {
                 console.log("no mutual trust")
                 const connectionsToKill = [];
                 for(let [host, connection] of establishedConnections){
+                    console.log(clientToUser.get(connection.ws))
                     if(clientToUser.get(host).uuid === user.uuid && clientToUser.get(connection.ws).uuid === userEntry.uuid){
                         connectionsToKill.push({a: host, b: connection.ws, id: connection.id})
                     }
